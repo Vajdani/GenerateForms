6e6343ca20fdd488116aa4e5024c0bea9e65bedf
Vajdani <50171679+Vajdani@users.noreply.github.com>
2024-11-27 09:37:20 +0100

    Add files via upload

---
+++ generate_forms/app.js
@@ -0,0 +1,74 @@
+import { renderField, renderForm } from "./field.js"
+
+const app = document.getElementById("app")
+let globalState = {}
+
+// let text1 = renderField({
+//     id: "text1",
+//     type: "text",
+//     state: globalState,
+//     label: "ez itt most az első text",
+//     labelOnTop : true,
+// })
+// app.appendChild(text1)
+
+// app.appendChild(document.createElement("br"))
+// app.appendChild(document.createElement("br"))
+// let text2 = renderField({
+//     id: "text2",
+//     type: "text",
+//     state: globalState,
+//     label: "ez itt most a második text",
+// })
+// app.appendChild(text2)
+
+// app.appendChild(document.createElement("br"))
+// app.appendChild(document.createElement("br"))
+// let number = renderField({
+//     id: "number1",
+//     type: "number",
+//     state: globalState,
+//     label: "ez itt most az első number",
+// })
+// app.appendChild(number)
+
+
+// app.appendChild(document.createElement("br"))
+// app.appendChild(document.createElement("br"))
+// let email1 = renderField({
+//     id: "email1",
+//     type: "email",
+//     state: globalState,
+//     label: "ez itt most az első ímél",
+//     labelOnTop : true,
+// })
+// app.appendChild(email1)
+
+
+let form = renderForm({
+    showState: true,
+    labelOnTop: true,
+    fields: [
+        {
+            id: "text1",
+            type: "text",
+            label: "ez itt most az első text",
+        },
+        {
+            id: "text2",
+            type: "text",
+            label: "ez itt most a második text",
+        },
+        {
+            id: "number1",
+            type: "number",
+            label: "ez itt most az első number",
+        },
+        {
+            id: "email1",
+            type: "email",
+            label: "ez itt most az első ímél",
+        }
+    ]
+})
+app.appendChild(form)
---
+++ generate_forms/field.js
@@ -0,0 +1,157 @@
+export function renderField(obj) {
+    switch (obj.type) {
+        case "text":
+            return renderText(obj)
+        case "number":
+            return renderNumber(obj)
+        case "email":
+            return renderEmail(obj)
+        case "default":
+            return renderDefault(obj)
+    }
+}
+
+function renderText(obj) {
+    return renderLabel(obj, (obj) => {
+        let field = document.createElement("input")
+        field.id = obj.id
+        field.type = "text"
+
+        field.addEventListener("input", (event) => {
+            obj.state[obj.id] = field.value
+        })
+
+        return field
+    })
+}
+
+function renderNumber(obj) {
+    return renderLabel(obj, (obj) => {
+        let field = document.createElement("input")
+        field.id = obj.id
+        field.type = "number"
+
+        field.addEventListener("input", (event) => {
+            let value = field.value
+
+            // if (field.value == "" && obj.state[obj.id].length > 1) {
+            //     field.value = obj.state[obj.id]
+            //     return
+            // }
+
+            obj.state[obj.id] = value
+            field.value = value
+        })
+
+        return field
+    })
+}
+
+function renderEmail(obj) {
+    return renderLabel(obj, (obj, container) => {
+        let _container = container
+        if (_container == null) {
+            _container = document.createElement("div")
+            _container.id = obj.id
+        }
+
+        let name = document.createElement("input")
+        name.id = obj.id + "_name"
+        name.type = "text"
+        name.style.display = "inline"
+
+        name.addEventListener("input", (event) => {
+            obj.state[obj.id] = name.value
+        })
+
+        _container.appendChild(name)
+
+        let kukac = document.createElement("p")
+        kukac.innerText = "@"
+        kukac.style.display = "inline"
+
+        _container.appendChild(kukac)
+
+        let domain = document.createElement("input")
+        domain.id = obj.id + "_domain"
+        domain.type = "text"
+        domain.style.display = "inline"
+
+        domain.addEventListener("input", (event) => {
+            obj.state[obj.id] = domain.value
+        })
+
+        _container.appendChild(domain)
+
+        if (container == null) {
+            return _container
+        }
+    })
+}
+
+function renderDefault(obj) {
+    let field = document.createElement("div")
+    field.id = obj.id
+
+    return field
+}
+
+function renderLabel(obj, createFunc) {
+    let container
+    if (obj.label != null) {
+        container = document.createElement("div")
+
+        let label = document.createElement("p")
+        label.for = obj.id
+        label.innerText = obj.label
+        label.id = "label_" + obj.id
+
+        if (obj.labelOnTop == true) {
+            label.style.display = "block"
+            label.style.padding = 0
+            label.style.margin = 0
+        }
+        else {
+            label.style.display = "inline"
+        }
+
+        container.appendChild(label)
+    }
+
+    let field = createFunc(obj, container)
+
+    if (container != null) {
+        if (field != null) {
+            container.appendChild(field)
+        }
+
+        return container
+    }
+
+    return field
+}
+
+export function renderForm(form) {
+    let _form = document.createElement("div")
+    var formState = {}
+
+    if (form.showState) {
+        let label = document.createElement("p")
+        label.id = "form_state"
+        label.innerText = "Form state: "
+        _form.appendChild(label)
+    }
+
+    for (let index = 0; index < form.fields.length; index++) {
+        let config = form.fields[index]
+        config.labelOnTop = form.labelOnTop
+        config.state = formState
+        _form.appendChild(renderField(config))
+        
+        // _form.getElementById(config.id).addEventListener("input", (event) => {
+        //     document.getElementById("form_state").innerText = JSON.stringify(obj.state)
+        // })
+    }
+
+    return _form
+}
